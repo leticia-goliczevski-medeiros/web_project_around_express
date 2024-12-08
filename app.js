c33fbdc5bd7b5a362adc0dcf3268a6bccd69b39d
leticia-goliczevski-medeiros <leticia_goliczevski@outlook.com>
2024-12-08 09:33:02 -0300

    feat: configure cards rout

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
-const express = require('express')
+import express from 'express';
 const app = express()
 const { PORT } = process.env
+import router from './routes/cards'
+
+app.use(express.json())
+/* quando uma solicitação com Content-Type do tipo application/json for recebida, express.json() vai converter o dados JSON em um objeto JS e deixá-lo disponível em req.body
+ */
+app.use('/cards', router)
 
 app.listen(PORT, ()=> {
   console.log(`App listening at port ${PORT}`)
---
+++ routes/cards.js
@@ -0,0 +1,9 @@
+import express from 'express'
+import path from 'path';
+import {cards} from `${path.join(__dirname, 'cards.json')}` assert { type: 'json' };
+
+const router = express.Router()
+
+router.get('/cards', (req, res)=> {
+  res.send(cards)
+})
