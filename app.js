febb32026f6df0cce2ce02b39f27c568153a49fe
leticia-goliczevski-medeiros <leticia_goliczevski@outlook.com>
2024-12-08 10:36:23 -0300

    feat: add global middleware to handle unmatched routes

--- app.js
+++ app.js
@@ -7,6 +7,10 @@ import userRouter from './routes/users'
 app.use(express.json())
 /* quando uma solicitação com Content-Type do tipo application/json for recebida, express.json() vai converter o dados JSON em um objeto JS e deixá-lo disponível em req.body
  */
+
+app.use('/', (req, res)=> {
+  res.status(404).send({ "message":"A solicitação não foi encontrada" })
+})
 app.use('/cards', router)
 app.use('/users', userRouter)
 
