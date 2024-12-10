c0dbccbc4ea2ad20390249853a75291bad98e223
leticia-goliczevski-medeiros <leticia_goliczevski@outlook.com>
2024-12-10 10:24:41 -0300

    feat: handle json files

--- routes/cards.js
+++ routes/cards.js
@@ -1,11 +1,30 @@
 import express from 'express'
 import path from 'path';
-import {cards} from `${path.join(__dirname, 'cards.json')}` assert { type: 'json' };
+import fs from 'fs'
+
+const __dirname = import.meta.dirname;
+const cardsPath = path.join(__dirname, '..', 'data', 'cards.json')
+
+let cards = []
+
+fs.readFile(cardsPath, (error, data)=> {
+  if (error) {
+    console.log(error)
+    cards = {error: 'Não foi possível ler o arquivo'}
+    return
+  }
+
+  cards = JSON.parse(data)
+})
 
 const router = express.Router()
 
 router.get('/cards', (req, res)=> {
-  res.send(cards)
+  if (cards.error) {
+    return res.status(404).json(cards)
+  }
+
+  return res.json(cards)       
 })
 
 export default router
