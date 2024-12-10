f104e46d39687af77ed7b40af1bfa116c1f92150
leticia-goliczevski-medeiros <leticia_goliczevski@outlook.com>
2024-12-10 10:46:56 -0300

    feat: configure users routes

--- routes/users.js
+++ routes/users.js
@@ -1,21 +1,46 @@
 import express from 'express'
-import path from 'path';
-import {users} from `${path.join(__dirname, 'users.json')}` assert { type: 'json' };
+import path from 'path'
+import fs from 'fs'
+
+const __dirname = import.meta.dirname;
+const usersPath = path.join(__dirname, '..', 'data', 'users.json')
+
+let users = []
+
+fs.readFile(usersPath, (error, data)=> {
+  if (error) {
+    console.log(error)
+    users = {error: 'Não foi possível ler o arquivo'}
+    return
+  }
+
+  users = JSON.parse(data)
+})
 
 const userRouter = express.Router();
 
-userRouter.get('/users', (req, res)=> {
-  res.send(users)
+userRouter.get('/', (req, res)=> {
+  if (users.error) {
+    return res.status(404).json(users)
+  }
+
+  return res.json(users)
 })
 
-userRouter.get('/users/:id', (req, res)=> {
-  const { _id } = req.params
+userRouter.get('/:id', (req, res)=> {
+  const { id } = req.params
 
-  const user = users.find( currentUser => currentUser._id === _id)
+  const user = users.find( currentUser => currentUser.id === id)
 
-  if (!user || !_id) {
-    res.status(404).send({ "message": "ID do usuário não encontrado" })
+  if (users.error) {
+    return res.status(404).json(cards)
   }
+
+  if (!user) {
+    res.status(404).send({ "message": `ID do usuário ${id} não encontrado` })
+  }
+
+  return res.json(users.id)
 })
 
 export default userRouter
