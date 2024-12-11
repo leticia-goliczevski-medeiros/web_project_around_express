cd5709d51cf4988884cb84d95a9ec2a01ec919a1
leticia-goliczevski-medeiros <leticia_goliczevski@outlook.com>
2024-12-11 20:27:47 -0300

    refactor: define PORT

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 import express from 'express';
 const app = express()
-const { PORT = 3000 } = process.env
+const PORT = 3000
 import cardsRouter from './routes/cards.js'
 import userRouter from './routes/users.js'
 
@@ -8,12 +8,12 @@ app.use(express.json())
 /* quando uma solicitação com Content-Type do tipo application/json for recebida, express.json() vai converter o dados JSON em um objeto JS e deixá-lo disponível em req.body
  */
 
-app.use('/', (req, res)=> {
+/* app.use('/', (req, res)=> {
   res.status(404).send({ "message":"A solicitação não foi encontrada" })
-})
+}) */
 app.use('/cards', cardsRouter)
 app.use('/users', userRouter)
 
-app.listen(3000, ()=> {
-  console.log(`App listening at port ${3000}`)
+app.listen(PORT, ()=> {
+  console.log(`App listening at port ${PORT}`)
 })
