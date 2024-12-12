c20f6cbc4455215914c11df3ac10800fd42684a6
leticia-goliczevski-medeiros <leticia_goliczevski@outlook.com>
2024-12-12 09:17:39 -0300

    refactor: reorder routers and imports

--- app.js
+++ app.js
@@ -1,19 +1,20 @@
 import express from 'express';
-const app = express()
-const PORT = 3000
-import cardsRouter from './routes/cards.js'
-import userRouter from './routes/users.js'
+import cardsRouter from './routes/cards.js';
+import userRouter from './routes/users.js';
 
-app.use(express.json())
-/* quando uma solicitação com Content-Type do tipo application/json for recebida, express.json() vai converter o dados JSON em um objeto JS e deixá-lo disponível em req.body
+const app = express();
+const PORT = 3000;
+
+app.use(express.json());
+/* quando uma solicitação com Content-Type application/json for recebida, express.json() vai converter o dados JSON em um objeto JS e deixá-lo disponível em req.body
  */
 
-/* app.use('/', (req, res)=> {
-  res.status(404).send({ "message":"A solicitação não foi encontrada" })
-}) */
-app.use('/cards', cardsRouter)
-app.use('/users', userRouter)
+app.use('/cards', cardsRouter);
+app.use('/users', userRouter);
+app.use('/', (req, res) => {
+  res.status(404).send({ 'message': 'A solicitação não foi encontrada' });
+});
 
-app.listen(PORT, ()=> {
-  console.log(`App listening at port ${PORT}`)
-})
+app.listen(PORT, () => {
+  console.log(`App listening at port ${PORT}`);
+});
