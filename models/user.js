deac02e94db2f26f90c6c1b77b880a0276b995f2
leticia-goliczevski-medeiros <leticia_goliczevski@outlook.com>
2025-01-14 17:19:30 -0300

    feat: create card and user models

---
+++ models/user.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    minlength: 2,
+    maxlength: 30
+  },
+  about: {
+    type: String,
+    required: true,
+    minlength: 2,
+    maxlength: 30
+  },
+  avatar: {
+    type: String,
+    required: true,
+    validate: {
+      validator(v) {
+        return /https?:\/\/(www\.)?.{1,}/gm.test(v)
+      },
+      message: 'É necessário um link válido.'
+    }
+  }
+})
+
+module.exports = mongoose.model('user', userSchema);
