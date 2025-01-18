91cb3bd76ae01a2ce96875b61045e664d4b4fc35
leticia-goliczevski-medeiros <leticia_goliczevski@outlook.com>
2025-01-18 12:27:42 -0300

    refactor: remove unused file reading function

--- routes/users.js
+++ routes/users.js
@@ -1,24 +1,5 @@
 const express = require('express');
 const {getUsers, getUser, createUser} = require('../controllers/users')
-const path = require('path');
-const fs = require('fs');
-
-const usersPath = path.join(__dirname, '..', 'data', 'users.json');
-
-function readUsers() {
-  let users = [];
-
-  fs.readFile(usersPath, (error, data) => {
-    if (error) {
-      console.log(error);
-      users = { error: 'Não foi possível ler o arquivo' };
-      return;
-    }
-
-    users = JSON.parse(data);
-  });
-  return users;
-}
 
 const userRouter = express.Router();
 
