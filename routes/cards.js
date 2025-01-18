c36473fa3b3315a6a1ae1ed93deed8c8de7c47c2
leticia-goliczevski-medeiros <leticia_goliczevski@outlook.com>
2025-01-18 13:09:35 -0300

    refactor: remove unused file reading function at routes/cards.js

--- routes/cards.js
+++ routes/cards.js
@@ -1,29 +1,9 @@
 const express = require('express');
-const path = require('path');
-const fs = require('fs');
-
-const cardsPath = path.join(__dirname, '..', 'data', 'cards.json');
-
-function readCards() {
-  let cards = [];
-
-  fs.readFile(cardsPath, (error, data) => {
-    if (error) {
-      console.log(error);
-      cards = { error: 'Não foi possível ler o arquivo' };
-      return;
-    }
-
-    cards = JSON.parse(data);
-  });
-
-  return cards;
-}
 
 const cardsRouter = express.Router();
 
 cardsRouter.get('/', (req, res) => {
-  const cards = readCards();
+  // const cards = readCards();
   if (cards.error) {
     return res.status(404).json(cards);
   }
