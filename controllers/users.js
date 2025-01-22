1bd83eb4ee3677115d6f2c51b3fcb0040d80dc47
leticia-goliczevski-medeiros <leticia_goliczevski@outlook.com>
2025-01-22 19:05:33 -0300

    feat: improve data validation in user controllers

--- controllers/users.js
+++ controllers/users.js
@@ -29,6 +29,11 @@ function getUser(req, res) {
 function createUser(req, res) {
   const { name, about, avatar } = req.body;
 
+  if (!name || !about || !avatar) {
+    res.status(INVALID_DATA).send({ message: 'Não foi possível criar o usuário. Dados incompletos.' });
+    return;
+  }
+
   const avatarRegex = /https?:\/\/(www\.)?.{1,}/;
   const isAvatarValid = avatar.match(avatarRegex);
 
@@ -49,6 +54,11 @@ function updateProfileInfo(req, res) {
   const userId = req.user._id;
   const { name, about } = req.body;
 
+  if (!name || !about) {
+    res.status(INVALID_DATA).send({ message: 'Não foi possível atualizar os dados do usuário. Dados incompletos.' });
+    return;
+  }
+
   User.findByIdAndUpdate(userId, { name, about }, {
     new: true,
     runValidators: true,
@@ -65,6 +75,11 @@ function updateProfileInfo(req, res) {
 function updateProfileAvatar(req, res) {
   const userId = req.user._id;
   const { avatar } = req.body;
+
+  if (!avatar) {
+    res.status(INVALID_DATA).send({ message: 'Não foi possível atualizar a foto do usuário. Dados incompletos.' });
+    return;
+  }
 
   const avatarRegex = /https?:\/\/(www\.)?.{1,}/;
   const isAvatarValid = avatar.match(avatarRegex);
